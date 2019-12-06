c0e2fd84789d042bda24afed2d7d6aea1b3ccd8b
Wesley Ellis <tahnok@gmail.com>
2019-12-06 07:34:30 -0500

    clean up template

---
+++ python/day_00_1.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test_test(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == "__main__":
+    with open("inputs/day00.txt") as f:
+        source = f.read()
+    print("TODO")
